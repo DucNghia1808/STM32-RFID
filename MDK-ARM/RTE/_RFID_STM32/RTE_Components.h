022d61e2277414096eb196309bfdf7e234a4aec5
Nghia Nguyen <84086601+DucNghia1808@users.noreply.github.com>
2022-06-26 20:27:43 +0700

    first commit cua nghia
    Signed-off-by: Nghia Nguyen <84086601+DucNghia1808@users.noreply.github.com>

---
+++ MDK-ARM/RTE/_RFID_STM32/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'RFID_STM32' 
+ * Target:  'RFID_STM32' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
